729856b93a38ec81ec684b99faf8436c7bae838a
Otavio Simoncini <contato.otaviosimoncini@gmail.com>
2025-01-21 10:17:08 -0300

    Bank Account complete

--- src/Exercises/BankAccount.java
+++ src/Exercises/BankAccount.java
@@ -31,10 +31,43 @@ public class BankAccount {
 
         int option = 0;
         while (option != 4) {
-            System.out.println("*****************************************");
-            System.out.println("Choose an option:\n1- Check balance\n2- Receive transfer\n3- Transfer\n4- Exit");
-            System.out.println("*****************************************");
+            System.out.println("*****************************************\nChoose an option:\n1- Check balance\n2- Receive transfer\n3- Transfer\n4- Exit\n*****************************************");
             option = scanner.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.println("Your balance is $ " + balance + "\n1- Back");
+                    int back = scanner.nextInt();
+
+                    if (back == 1){
+                        break;
+                    }
+                case 2:
+                    System.out.println("Fill the value that you received");
+                    double receivedValue = scanner.nextDouble();
+
+                    balance += receivedValue;
+
+                    System.out.println("Transfer received succesfully\n1- Back");
+                    System.out.println("");
+
+                    int backCaseTwo = scanner.nextInt();
+                    if (backCaseTwo == 1){
+                        break;
+                    }
+                case 3:
+                    System.out.println("Fill the value that you want transfer");
+                    double transferedValue = scanner.nextDouble();
+
+                    balance -= transferedValue;
+
+                    System.out.println("Transfer made succesfully\n1- Back");
+
+                    int backCaseThree = scanner.nextInt();
+                    if (backCaseThree == 1){
+                        break;
+                    }
+            }
         }
     }
 }
